529aefc7e14ec5a10d729bccfff8882765e0fd69
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-16 11:09:30 +0330

    initial commit

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hassanzadeh/Python/WizeAnalytics_Project/wizello/ENV/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
