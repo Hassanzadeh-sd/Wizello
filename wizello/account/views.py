70ff946d33c8372c8ce99253cc3e541efc19a35e
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-17 01:30:21 +0330

    Account Views list and Deactive

--- wizello/account/views.py
+++ wizello/account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.views.generic import ListView, View
+from .models import Employee, User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+# -------------------- Account List
+class AccountListView(LoginRequiredMixin, ListView):
+    model = Employee
+    context_object_name = "employees"
+    template_name = "account/accountlist.html"
+
+
+class AccountDeactiveView(LoginRequiredMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        obj_employee = get_object_or_404(Employee, pk=pk)
+        # check permission
+        obj_employee.organization = 1
+        obj_employee.save()
+        return HttpResponseRedirect(reverse_lazy("account:accountlist"))
