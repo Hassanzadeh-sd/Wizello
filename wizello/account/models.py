fb83a2539c1ceff6aff5c87e04ae9036ada08557
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-16 22:04:01 +0330

    add employee model

--- wizello/account/models.py
+++ wizello/account/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from core.models import BaseModel, models
+from django.contrib.auth.models import User
+from organization.models import Organization
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Employee(BaseModel):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="employee")
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
+    def __str__(self):
+        return self.user.username
+
+#    def get_absolute_url(self):
+#        return reverse("employee_detail", kwargs={"pk": self.pk})
