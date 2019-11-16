ab6af1e3a22059c77321744785d67e342dc84a68
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-16 15:48:22 +0330

    create organization model

--- wizello/organization/models.py
+++ wizello/organization/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from core.models import BaseModel, models
 
-# Create your models here.
+
+class Organization(BaseModel):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Organization"
+        verbose_name_plural = "Organizations"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+#    def get_absolute_url(self):
+#        return reverse("organization_detail", kwargs={"pk": self.pk})
