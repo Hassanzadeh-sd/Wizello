9dbba271df6a8355942c7d937786aa11d1099429
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-17 09:07:38 +0330

    create register form base

---
+++ wizello/account/forms.py
@@ -0,0 +1,31 @@
+
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label="User Name")
+    email = forms.EmailField(label="Email")
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(
+        widget=forms.PasswordInput, label="Password Confirm")
+    organization = forms.CharField(label="organization")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username__icontains=username).exists():
+            raise forms.ValidationError("The username is token")
+        return username
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if (password1 != password2):
+            raise forms.ValidationError("password confirm not same")
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email__icontains=email).exists():
+            raise forms.ValidationError("The email is already registered")
+        return email
--- wizello/account/views.py
+++ wizello/account/views.py
@@ -4,9 +4,11 @@ from .models import Employee, User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.urls import reverse_lazy
-
+from .forms import RegisterForm
 
 # -------------------- Account List
+
+
 class AccountListView(LoginRequiredMixin, ListView):
     model = Employee
     context_object_name = "employees"
@@ -20,3 +22,29 @@ class AccountDeactiveView(LoginRequiredMixin, View):
         obj_employee.organization = 1
         obj_employee.save()
         return HttpResponseRedirect(reverse_lazy("account:accountlist"))
+
+
+# -------------------- Register
+class RegisterView(FormView):
+    template_name = 'registration/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy("core:login")
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password1 = form.cleaned_data['password1']
+        new_user = User.objects.create(username=username, email=email)
+        new_user.set_password(password1)
+        new_user.save()
+
+        obj_new_user = User.objects.get(username=username)
+        obj_user_employee, created = Employee.objects.get_or_create(
+            user=obj_new_user)
+        try:
+            obj_user_employee.organization = form.cleaned_data['organization']
+        except:
+            pass
+        obj_user_employee.save()
+
+        return super(RegisterView, self).form_valid(form)
