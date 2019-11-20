ac0f02d4947c9dfa8423e898e23a57debd0f2ca9
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-20 14:53:59 +0330

    create request adminlist and accept views

--- wizello/request/urls.py
+++ wizello/request/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path, include
 from .views import (
     RequestManagerListView, RequestManagerAcceptView,
-    RequestListView, RequestCreateView, RequestDeleteView)
+    RequestListView, RequestCreateView, RequestDeleteView,
+    RequestAdminListView, RequestAdminAcceptView)
 
 app_name = 'request'
 
 urlpatterns = [
     # request
     path('', RequestListView.as_view(), name="requestlist"),
-    path('manager/', RequestManagerListView.as_view(), name="requestmanagerlist"),
     path('create/', RequestCreateView.as_view(), name="requestcreate"),
+    path('delete/<int:pk>/', RequestDeleteView.as_view(), name="requestdelete"),
+    # Manager
+    path('manager/', RequestManagerListView.as_view(), name="requestmanagerlist"),
     path('manager/accept/<int:pk>/',
          RequestManagerAcceptView.as_view(), name="requestaccept"),
-    path('delete/<int:pk>/', RequestDeleteView.as_view(), name="requestdelete"),
+    # Admin
+    path('admin/', RequestAdminListView.as_view(), name="requestadminlist"),
+    path('admin/accept/<int:pk>/',
+         RequestAdminAcceptView.as_view(), name="requestadminaccept"),
 ]
--- wizello/request/views.py
+++ wizello/request/views.py
@@ -3,22 +3,11 @@ from django.views.generic import ListView, CreateView, View, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.urls import reverse_lazy
+from django.db.models import Q
 from .models import Request, User
 from account.models import Employee
 
 # -------------------- Request List
-
-
-class RequestManagerListView(LoginRequiredMixin, ListView):
-    model = Request
-    context_object_name = "requests"
-    template_name = "request/requestmanagerlist.html"
-
-    def get_queryset(self):
-        organization = self.request.user.employee.organization
-        qs = Request.objects.filter(
-            organization=organization).exclude(user=self.request.user)
-        return qs
 
 
 class RequestListView(LoginRequiredMixin, ListView):
@@ -45,6 +34,26 @@ class RequestCreateView(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(self.get_success_url())
 
 
+class RequestDeleteView(LoginRequiredMixin, DeleteView):
+    model = Request
+    template_name = "core/confirm_delete.html"
+    success_url = reverse_lazy("request:requestlist")
+
+# -------------------- Request Manager List
+
+
+class RequestManagerListView(LoginRequiredMixin, ListView):
+    model = Request
+    context_object_name = "requests"
+    template_name = "request/requestmanagerlist.html"
+
+    def get_queryset(self):
+        organization = self.request.user.employee.organization
+        qs = Request.objects.filter(
+            organization=organization).exclude(user=self.request.user).exclude(Q(user__employee__organization=organization) | Q(position="M"))
+        return qs
+
+
 class RequestManagerAcceptView(LoginRequiredMixin, View):
     def get(self, request, pk, *args, **kwargs):
         obj_request = get_object_or_404(Request, pk=pk)
@@ -59,7 +68,28 @@ class RequestManagerAcceptView(LoginRequiredMixin, View):
         return HttpResponseRedirect(reverse_lazy("request:requestmanagerlist"))
 
 
-class RequestDeleteView(LoginRequiredMixin, DeleteView):
+# -------------------- Request Admin List
+
+class RequestAdminListView(LoginRequiredMixin, ListView):
     model = Request
-    template_name = "core/confirm_delete.html"
-    success_url = reverse_lazy("request:requestlist")
+    context_object_name = "requests"
+    template_name = "request/requestadminlist.html"
+
+    def get_queryset(self):
+        qs = Request.objects.filter(
+            position="M").filter(user__employee__organization=None)
+        return qs
+
+
+class RequestAdminAcceptView(LoginRequiredMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        obj_request = get_object_or_404(Request, pk=pk)
+        obj_request.agreement = timezone.now()
+
+        objEmployee = Employee.objects.get(user=obj_request.user)
+        objEmployee.position = obj_request.position
+        objEmployee.organization = obj_request.organization
+        objEmployee.save()
+
+        obj_request.save()
+        return HttpResponseRedirect(reverse_lazy("request:requestadminlist"))
