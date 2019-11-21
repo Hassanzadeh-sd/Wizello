37f7f83a4227dda4ad757bcd6d52b471b8202fe5
“hassanzadeh-sd” <hassanzadeh.sd@gmail.com>
2019-11-21 14:06:25 +0330

    create API Employee Deactive For Admin User

--- wizello/account/api/views.py
+++ wizello/account/api/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,7 +20,13 @@ class EmployeeUpdateAPIView(generics.UpdateAPIView):
     permission_classes = [permissions.IsAdminUser]
 
 
-class EmployeeDeactiveAPIView(generics.DestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeModelSerializer
-    permission_classes = [permissions.IsAdminUser]
+class EmployeeDeactiveAPIView(APIView):
+    permission_classes = (permissions.IsAdminUser,)
+
+    def get(self, request, pk, format=None):
+        obj_employee = get_object_or_404(Employee, pk=pk)
+        obj_employee.organization = None
+        obj_employee.position = "E"
+        obj_employee.save()
+        message = "Deactive User"
+        return Response({"message": message}, status=400)
